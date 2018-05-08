8da224ee616964aefb4acdbc17fabed7d63f898c
zainul franciscus <zainul.franciscus@gmail.com>
2018-05-08 19:26:02 +1000

    add an environment variable and integration test
    Location of this json file is kept in an environment variable
    I also add a browser-based test using TestCafe. This test
    is intended as an integration test, which ensures that
    the app is working as intended.

---
+++ integration-test/test.js
@@ -0,0 +1,27 @@
+import {Selector} from 'testcafe';
+
+
+fixture `Adding houses into the right column`
+    .page `http://localhost:3000/`;
+
+test('Clicking on an add button, adds house to the right column', async t => {
+
+    const house = Selector('#container-1');
+    const addBtn = '#move-btn-1';
+    const houseInRightColumn = Selector('#right-column #container-1');
+    await t
+        .hover(house)
+        .click(addBtn)
+        .expect(houseInRightColumn.count).eql(1);
+});
+
+test('Clicking on a remove button, removes a house in right column', async t => {
+
+    const house = Selector('#container-4');
+    const removeBtn = '#move-btn-4';
+    const houseInRightColumn = Selector('#right-column #container-4');
+    await t
+        .hover(house)
+        .click(removeBtn)
+        .expect(houseInRightColumn.count).eql(0);
+});
